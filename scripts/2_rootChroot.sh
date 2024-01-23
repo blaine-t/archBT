80213d5a4866160c86acd1a2e6a4a2b989f69cf8
blaine-t <blaine-t@bathost.net>
2024-01-23 12:11:32 -0600

    Initial power savings update

--- scripts/2_rootChroot.sh
+++ scripts/2_rootChroot.sh
@@ -165,6 +165,7 @@ fi
 cat << EOF
 
 If you messed up your user password now is the time to fix that.
+The cmdline supplied in this script is intended for laptops. If you don't need laptop powersavings feel free to delete after bgrt_disable.
 Otherwise you can just Ctrl+D reboot or run 3_wmde.sh to install a minimal KDE Plasma Desktop install.
 Switching to user...
 
---
+++ scripts/5_powersaving.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Bash Strict Mode [aaron maxwell](http://redsymbol.net/articles/unofficial-bash-strict-mode/)
+set -euo pipefail
+IFS=$'\n\t'
+
+# Force script PWD to be where the script is located up a directory
+cd ${0%/*}
+cd ..
+
+# Update packages
+sudo pacman -Syu --noconfirm
+
+# Install optional dependencies
+sudo pacman -S acpid hdparm --noconfirm
+sudo systemctl enable --now acpid
+
+# Supress normal keypresses from logs
+cp config/acpi/* /etc/acpi/events/
+
+# laptop-mode-tools setup
+echo '1' | paru -a laptop-mode-tools --skipreview --noconfirm
+cp -r config/lm/* /etc/laptop-mode/
