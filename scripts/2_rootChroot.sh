3d5f5592067e75a1b12095f8505b5d9b519c7c69
blaine-t <blaine-t@bathost.net>
2024-03-15 11:48:27 -0500

    Test Run 1

--- scripts/2_rootChroot.sh
+++ scripts/2_rootChroot.sh
@@ -126,8 +126,9 @@ while [[ ${boot_entries} -gt 0 ]]; do
 done
 
 # Copy over bcachefs hook and install
-cp mkinitcpio/hooks/bcachefs /etc/initcpio/hooks/
-cp mkinitcpio/install/bcachefs /etc/initcpio/install/
+# Hopefully no more need for this?
+# cp mkinitcpio/hooks/bcachefs /etc/initcpio/hooks/
+# cp mkinitcpio/install/bcachefs /etc/initcpio/install/
 
 # Regenerate the initramfs
 mkinitcpio -P
